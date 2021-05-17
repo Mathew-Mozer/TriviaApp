ab6cc37e642782a7b6208bc031071015b2ff5907
Mathew Mozer <zhydian@gmail.com>
2021-05-16 20:50:21 -0400

    Styled HomeScreen

--- screens/home/index.js
+++ screens/home/index.js
@@ -1,15 +1,49 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
 import PropTypes from 'prop-types'
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
+  const { navigate } = navigation
   return (
-    <View>
-      <Text>Hello Home</Text>
+    <View style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Text style={[styles.textBox]}>You will be presented</Text>
+        <Text style={styles.textBox}>with 10 True or False</Text>
+        <Text style={styles.textBox}>Questions</Text>
+      </View>
+      <View style={{ flex: 2 }}>
+        <Text style={styles.textBox}>Can you score 100%</Text>
+      </View>
+      <View style={styles.footer}>
+        <TouchableOpacity onPress={() => navigate('Home', { tes: '' })}>
+          <Text style={styles.buttonText}>Begin</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
 
-HomeScreen.propTypes = {}
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 4,
+  },
+  textBox: {
+    textAlign: 'center',
+    fontSize: 30,
+  },
+  footer: { padding: 20 },
+  buttonText: {
+    fontSize: 30,
+    textAlign: 'center',
+  },
+})
+
+HomeScreen.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
+}
 
 export default HomeScreen
