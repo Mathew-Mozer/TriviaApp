6ea4e89bed197c63c20376ffd5b230d5d10f710f
Mathew Mozer <zhydian@gmail.com>
2021-06-13 17:33:25 -0400

    Connected to api

--- hooks/useQuestions/index.js
+++ hooks/useQuestions/index.js
@@ -1,35 +1,52 @@
 import { useState } from 'react'
+import axios from 'axios'
 import { useQuiz } from '../../providers/QuizProvider'
 import * as he from 'he'
 import { addQuestions } from '../../providers/QuizProvider/ActionCreators'
 
-const mockData = {
-  response_code: 0,
-  results: [
-    {
-      category: 'Mythology',
-      type: 'boolean',
-      difficulty: 'hard',
-      question:
-        'Rannamaari was a sea demon that haunted the people of the Maldives and had to be appeased monthly with the sacrifice of a virgin girl.',
-      correct_answer: 'True',
-      incorrect_answers: ['False'],
-    },
-  ],
-}
-
 export default useQuestions = () => {
   const { state, dispatch } = useQuiz()
-  const [questionsLoading, setQuestionsLoading] = useState(true)
+  const [questionsLoading, setQuestionsLoading] = useState(false)
   const [questionsError, setQuestionsError] = useState()
 
   // Get questions from server.
-  const getQuestions = () =>
+  const getQuestions = async () => {
+    setQuestionsLoading(true)
+    try {
+      const response = await axios.get(
+        'https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean'
+      )
+      const cleanData = response.data.results.map((q) => ({
+        ...q,
+        question: he.decode(q.question),
+      }))
+      dispatch(addQuestions(cleanData))
+      if (response.data.results.length < 1) {
+        throw Error('No Questions received. Please contact quiz administrator')
+      }
+
+      if (response.data.response_code != 0) {
+        throw Error('Error getting questions')
+      }
+      console.log('data', response.data)
+
+      setQuestionsError(null)
+    } catch (error) {
+      setQuestionsError(error.message)
+      //console.log(error)
+    } finally {
+      setQuestionsLoading(false)
+    }
+  }
+
+  /*
+
     new Promise((resolve, reject) => {
-      setQuestionsLoading(true)
+      
 
       // Mocking server response
-      setTimeout(() => {
+
+       setTimeout(() => {
         setQuestionsLoading(false)
         try {
           const cleanData = mockData.results.map((q) => ({
@@ -52,7 +69,7 @@ export default useQuestions = () => {
 
         resolve(mockData)
       }, 2000)
-    })
+    }) */
 
   const getQuestion = () => {
     return state.questions.find((question) => {
